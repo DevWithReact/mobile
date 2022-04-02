6ae38778c4afc330c9671a13a43ab626c87e2699
DevWithReact <atroxltd@gmail.com>
2022-04-02 19:50:45 +0300

    add images

--- App/utils/Images.js
+++ App/utils/Images.js
@@ -10,4 +10,12 @@ export default {
   ic_key: require('./../assets/images/ic_key.png'),
   ic_pedal: require('./../assets/images/ic_pedal.png'),
   ic_search: require('./../assets/images/ic_search.png'),
+
+  ic_key: require('./../assets/images/ic_key.png'),
+  
+  ic_piano_charge: require('./../assets/images/ic_piano_charge.png'),  
+  ic_piano_charge_grey: require('./../assets/images/ic_piano_charge_grey.png'),  
+  ic_battery_st: require('./../assets/images/ic_battery_st.png'),
+  ic_pedal_charge: require('./../assets/images/ic_pedal_charge.png'),
+  ic_pedal_charge_grey: require('./../assets/images/ic_pedal_charge_grey.png'),
 };
